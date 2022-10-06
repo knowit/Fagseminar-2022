64d74ab4a58a8a270362ce264983e1b2e1422f91
Tom Martin Roberg <tom.martin.roberg@knowit.no>
2022-10-06 14:40:08 +0200

    Added githubaction workflow

--- src/util/fetchUtil.ts
+++ src/util/fetchUtil.ts
@@ -2,7 +2,7 @@ import type { TimeRecord } from "../models/airtable";
 
 export async function getData() {
   const airtableUrl =
-    "https://api.airtable.com/v0/appYSYbiE8bAuHro3/innsendte_foredrag?view=Grid%20view";
+    "https://api.airtable.com/v0/appYSYbiE8bAuHro3/innsendte_foredrag_new_site?view=Grid%20view";
   return await fetch(airtableUrl, {
     headers: { Authorization: "Bearer key2Fr5HlESWRuuzx" },
   })
